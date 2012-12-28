9c12bb3923f6ad153f80fb1d2f953c4eb45d6411
Oliver Chu <j.oliverchu@berkeley.edu>
2012-12-28 00:11:14 -0800

    Create prototypeMain.php

---
+++ prototypeMain.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/styles.css" />
+<title>CrowdForce</title>
+</head>
+
+<body>
+<div class="center">
+<img src="crowdforceAwesomeFont.png" alt="CrowdForce" /><br />
+<span class="subtitle">Internet Tributes<br />
+Built by Humans in California</span><br /><br />
+</div>
+
+<div style="margin-left:350px">
+<small>How does this work exactly?</small><br /><br />
+
+<big>1. Let some people know what item or service you're are trying to find.<br />
+2. Make an Internet Tribute, a sort of monetary reward, for anyone who leads<br />you to your goal.</big><br /><br />
+
+<form action="" method="post">
+<input style="color:gray;font-size:2em;width:500px;height:30px;font-family:calibri,cambria,tahoma,verdana,arial,sans-serif"
+type="text" value="I am looking for ..." /><br />
+Examples: Girl who can make costumes of videogame characters, guy who can design a<br />custom operating system.<br /><br />
+<input style="color:gray;font-size:2em;width:500px;height:30px;font-family:calibri,cambria,tahoma,verdana,arial,sans-serif"
+type="text" value="I will give a reward of ..." /><br />
+Give people an incentive to help you find your goods or services. Examples: $40, $950.<br /><br />
+<input style="color:gray;font-size:2em;width:500px;height:100px;font-family:calibri,cambria,tahoma,verdana,arial,sans-serif"
+type="text" value="More specifically I am looking for ..." /><br />
+Write out a detailed explanation of what you want.<br /><br />
+
+<input style="color:gray;font-size:2em;width:500px;height:30px;font-family:calibri,cambria,tahoma,verdana,arial,sans-serif"
+type="text" value="My contact info is ..." /><br />
+Give your email or phone number. Skype name or Facebook page is also fine.<br />We will never give out your information.<br /><br />
+</form>
+
+<small>By submitting, you agree to pay on a successful find of whatever item or service you want.</small>
+
+<form action="bounty.php" method="POST">
+<input type="submit" value="Submit" />
+</form><br />
+<span style="font-size:17pt"><b>Explanation</b></span><br />
+<table class="smalltabl"><tr><td><span class="expla">CrowdForce tribute is a sum of money, like a bounty,<br />
+that you're willing to pay to get the thing you want,<br />
+whether it's an apartment, a job, or a slightly-used unicycle.<br />
+But a "bounty" typically goes to one person. A CrowdForce tribute can be<br />
+divided up among a whole crowd of people so that everyone has an<br />
+incentive to help you find what you're looking for.<br /><br />
+
+So how does it work?<br /><br />
+
+Let's take an example. Say Matt is looking for a one-bedroom apartment in New York,
+<br />and he's willing to pay $100 total to the person or people who<br />
+can help him find it. Matt creates a $100 CrowdForce Challenge.<br />
+The service then gives Matt a url that he can share with
+<br />friends over email or his social networks.<br /><br />
+
+Now let's say that Matt sends the url for his Challenge<br />
+to his friend Amanda. When Amanda clicks on the link, she<br />
+arrives at a CrowdForce page and sees two options:<br /><br />
+
+Option One:<br />
+If Amanda thinks she knows of an apartment for Matt,<br />
+she'll be able to email Matt via a unique email
+<br />address provided by the service and claim the money.<br /><br />
+
+Option Two:<br />
+If Amanda doesn't know of any apartments, she can still help --<br />
+and share in the CrowdForce -- by choosing to spread word about
+<br />Matt's apartment search with her own network. When Amanda
+<br />clicks one of the share buttons for Facebook, Twitter, or email,
+<br />she'll receive a unique url. This url is unique so that the
+<br />service can trace Amanda's role in the process and reward her
+<br />once Matt finds his apartment. When Amanda's friends receive
+<br />Amanda's link, they'll face the same options as Amanda.
+<br />They can either let Matt know about an apartment or continue to
+<br />spread the word via their own unique urls. The process continues
+<br />until Matt finds his apartment, at which point every person in<br />
+the chain is identified by his or her url and rewarded with<br />
+a share of the $100 CrowdForce Challenge.</span><br /><br />
+
+<small>CrowdForce is heavily inspired by <a target="_blank" href="http://www.crounty.com">the Crounty</a>.</small>
+
+</td></tr></table>
+<br /><br /><br /><br />
+
+<br /><br />
+</div>
+
+</body>
+</html>  
